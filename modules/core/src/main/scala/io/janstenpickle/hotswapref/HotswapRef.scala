b5770967eacfb363264bcd4f53043e21b700b6c6
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-06-11 11:29:36 +0300

    release lock if the resource has been swapped and is not needed

--- modules/core/src/main/scala/io/janstenpickle/hotswapref/HotswapRef.scala
+++ modules/core/src/main/scala/io/janstenpickle/hotswapref/HotswapRef.scala
@@ -34,9 +34,8 @@ trait HotswapRef[F[_], R] {
 
   /** Access `R` safely
     *
-    * Note that [[cats.effect.kernel.Resource]] `R` here is to maintain a count of the number of
-    * references that are currently open. A resource `R` with open references cannot be finalized and therefore
-    * cannot be fully swapped.
+    * Note that access to `R` is protected by a shared-mode lock via a [[cats.effect.kernel.Resource]] scope.
+    * A resource `R` with unreleased locks cannot be finalized and therefore cannot be fully swapped.
     */
   def access: Resource[F, R]
 }
@@ -46,17 +45,14 @@ object HotswapRef {
   /** Creates a new [[HotswapRef]] initialized with the specified resource. The [[HotswapRef]] instance is returned
     * within a [[cats.effect.kernel.Resource]]
     */
-  def apply[F[_]: Concurrent, R](initial: Resource[F, R]): Resource[F, HotswapRef[F, R]] = {
+  def apply[F[_], R](initial: Resource[F, R])(implicit F: Concurrent[F]): Resource[F, HotswapRef[F, R]] = {
 
-    /** Secure a resource by enriching it with a semaphore-based lock and a unique token and by modifying its `release`
-      * action.
+    /** Secure a resource by enriching it with a semaphore-based lock and a unique token and by modifying its finalizer.
       *
-      * The lock is acquired in shared mode during `access` to the resource (permits concurrent access but prohibits
-      * release) and in exclusive mode during `release` of the resource (prohibits access).
+      * The lock is acquired in shared mode when the resource is accessed (permits concurrent access but prohibits
+      * finalization) and in exclusive mode when the resource is finalized (prohibits access).
       *
-      * The token is used during `access` for consistent read of the holder reference.
-      *
-      * The `release` action of the resource is protected by an exclusive lock.
+      * The token is used during access for consistent read of the holder reference.
       */
     def secure(res: Resource[F, R]): Resource[F, (R, Lock[F], Unique.Token)] = for {
       lock <- Resource.eval(Lock[F])
@@ -78,14 +74,17 @@ object HotswapRef {
 
         /** Access to the resource is protected by a shared-mode lock. The holder reference is read at least twice:
           * first, to retrieve its content, and then, after acquiring the lock, to check if the content hasn't changed
-          * since the first read. If the holder has been swapped, the new content is passed to the next step of
-          * the loop. Otherwise, it's used to build the resulting `Resource`.
+          * since the first read. If the holder has been swapped, the lock is released and the new content is passed
+          * to the next step of the loop. Otherwise, it's used to build the resulting `Resource`.
           */
         val step: (R, Lock[F], Unique.Token) => F[Either[(R, Lock[F], Unique.Token), (R, F[Unit])]] =
           (r, lock, token) =>
-            lock.shared.allocated.flatMap { case (_, lockRelease) =>
-              holder.get.map { case (r1, lock1, token1) =>
-                Either.cond(token == token1, (r, lockRelease), (r1, lock1, token1))
+            F.uncancelable { poll: Poll[F] =>
+              poll(lock.shared.allocated).flatMap { case (_, lockRelease) =>
+                holder.get.flatMap { case (r1, lock1, token1) =>
+                  if (token != token1) lockRelease.as(Left((r1, lock1, token1)))
+                  else F.pure(Right((r, lockRelease)))
+                }
               }
             }
 
